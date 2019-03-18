6aba01952867deeafffc1aca4e93f59b6e9a5df9
QWp6t <hi@qwp6t.me>
2019-03-17 17:05:27 -0700

    Fix config file reference and exit early if no config exists

--- src/Clover/ServiceProvider.php
+++ src/Clover/ServiceProvider.php
@@ -16,12 +16,20 @@ abstract class ServiceProvider extends BaseServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(dirname(__DIR__) . "/config/{$this->meta->key}.php", $this->meta->key);
-        $providers = array_filter($this->app['config']->get("{$this->meta->key}.providers"), function ($provider) {
-            return $provider !== self::class;
+        // Exit early if no config file exists
+        if (!file_exists($configFile = dirname($this->meta->plugin) . "/config/{$this->meta->key}.php")) {
+            return;
+        }
+        $this->mergeConfigFrom($configFile, $this->meta->key);
+        $config = $this->app['config']->get($this->meta->key);
+        $providers = array_filter($config['providers'], function ($provider) {
+            return $provider !== static::class;
         });
         array_map([$this->app, 'register'], $providers);
         $this->app->singleton($this->meta->key, PluginName::class);
+        if ($this->app->bound('view') && isset($config['views'])) {
+            $this->loadViewsFrom($config['views'], $this->meta->key);
+        }
     }
 
     /**
