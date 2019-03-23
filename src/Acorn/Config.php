4da2f0b20fa6da28feeb934b097eef029384f9f8
QWp6t <hi@qwp6t.me>
2019-03-23 04:42:30 -0700

    Remove unused/unsupported features from Roots\Acorn\Config

--- src/Acorn/Config.php
+++ src/Acorn/Config.php
@@ -6,37 +6,5 @@ use Illuminate\Config\Repository as ConfigBase;
 
 class Config extends ConfigBase
 {
-    /** @var string[] List of directory paths containing configuration files */
-    public $paths = [];
-
-    /**
-     * Locate and merge a config file
-     *
-     * @param string $path
-     * @param string $key
-     */
-    public function load($path, $key = null)
-    {
-        $key    = ($key ?? basename($path, '.php'));
-        $config = $this->get($key, []);
-        $this->set($key, array_merge($config, require $path, require $this->locate($path)));
-    }
-
-    /**
-     * Locate config file based on specified paths
-     *
-     * @param string $path Path to a config file
-     */
-    protected function locate($path)
-    {
-        $file = basename($path);
-
-        foreach (array_unique($this->paths) as $dir) {
-            if (file_exists("{$dir}/{$file}")) {
-                return "{$dir}/{$file}";
-            }
-        }
-
-        return $path;
-    }
+    // this class is intentionally blank
 }
