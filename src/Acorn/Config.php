7d97aae00067d21a706918a844b5037c6744744a
QWp6t <QWp6t@users.noreply.github.com>
2019-03-23 16:16:21 -0700

    Apply WordPress filter to all configs

--- src/Acorn/Config.php
+++ src/Acorn/Config.php
@@ -6,5 +6,20 @@ use Illuminate\Config\Repository as ConfigBase;
 
 class Config extends ConfigBase
 {
-    // this class is intentionally blank
+    /**
+     * Set a given configuration value.
+     *
+     * @param  array|string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function set($key, $value = null)
+    {
+        $keys = is_array($key) ? $key : [$key => $value];
+
+        foreach ($keys as $key => $value) {
+            $value = apply_filters("roots.acorn.config.{$key}", $value);
+            Arr::set($this->items, $key, $value);
+        }
+    }
 }
