a01e8c8eb7a430595fee13c0ea346a136d90dfe3
QWp6t <hi@qwp6t.me>
2019-06-18 05:33:49 -0700

    [WIP] Check that .blade.php cannot be accessed remotely

--- src/Acorn/Sage/SageServiceProvider.php
+++ src/Acorn/Sage/SageServiceProvider.php
@@ -24,4 +24,35 @@ class SageServiceProvider extends ServiceProvider
             $this->app['sage']->attach();
         }
     }
+
+    public function preflight(\WP_Http $client)
+    {
+        $response = $client->request(get_theme_file_uri('resources/views/index.blade.php'), [
+            'method' => 'HEAD',
+            'sslverify' => ! $this->app->environment('local', 'testing', 'test', 'dev', 'development'),
+            'sslcertificates' => $this->getCertificate()
+        ]);
+
+        $status_code = wp_remote_retrieve_response_code($response);
+
+        if ($status_code >= 200 && $status_code < 300) {
+            throw new \Exception('i should not b abl 2 c bladez ya dummy');
+        }
+    }
+
+    /**
+     * Get CA bundle from common system locations.
+     *
+     * This follows a similar routine as Guzzle with notable
+     * fallback to CA bundle provided by WordPress.
+     *
+     * @return string Path to certificate
+     */
+    protected function getCertificate() : string
+    {
+        return ini_get('openssl.cafile')
+            ?: ini_get('curl.cainfo')
+            ?: openssl_get_cert_locations()['default_cert_file']
+            ?? \ABSPATH . \WPINC . '/certificates/ca-bundle.crt';
+    }
 }
