79b3c5dc1d7142f9f5d05e0c76d807277b9f81c2
QWp6t <hi@qwp6t.me>
2019-07-04 10:29:14 -0700

    Add Asset Manager

---
+++ src/Acorn/Assets/AssetsServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Roots\Acorn\Assets;
+
+use Roots\Acorn\ServiceProvider;
+
+class ManifestServiceProvider extends ServiceProvider
+{
+    /** {@inheritDoc} */
+    public function register()
+    {
+        $this->registerManager();
+        $this->registerManifests();
+    }
+
+    protected function registerManager()
+    {
+        $this->app->singleton('assets', function ($app) {
+            return new AssetsManager($app);
+        });
+    }
+
+    protected function registerManifests()
+    {
+        $this->app->singleton('assets.manifest', function () {
+            return $this->app->get('assets')->manifest();
+        });
+    }
+}
---
+++ src/Acorn/Assets/Contracts/Asset.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Roots\Acorn\Assets\Contracts;
+
+interface Asset
+{
+    /**
+     * Get the asset's remote URI
+     *
+     * Example: https://example.com/app/themes/sage/dist/styles/a1b2c3.min.css
+     *
+     * @return string
+     */
+    public function uri() : string;
+
+    /**
+     * Get the asset's local path
+     *
+     * Example: /srv/www/example.com/current/web/app/themes/sage/dist/styles/a1b2c3.min.css
+     *
+     * @return string
+     */
+    public function path() : string;
+
+    /**
+     * Check whether the asset exists on the file system
+     *
+     * @return bool
+     */
+    public function exists() : bool;
+
+    /**
+     * Get the contents of the asset
+     *
+     * @return string|false
+     */
+    public function contents();
+}
