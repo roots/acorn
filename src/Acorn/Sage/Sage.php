17699a00e8573bdcf86f177aaf5baa310d0cdbea
Brandon <brandon@tendency.me>
2019-07-29 01:46:45 -0500

    Seperate filters into their own functions Fix typo Various clean up / docblocks

--- src/Acorn/Sage/Sage.php
+++ src/Acorn/Sage/Sage.php
@@ -61,7 +61,7 @@ class Sage
         $this->files = $files;
         $this->fileFinder = $fileFinder;
         $this->sageFinder = $sageFinder;
-        $this->views = $view;
+        $this->view = $view;
     }
 
     /**
@@ -72,7 +72,7 @@ class Sage
     public function attach()
     {
         $this->attachPostVariable();
-        $this->attachCommentsTemplateResolver();
+        $this->attachCommentsTemplateFilter();
         $this->attachSearchFormFilter();
         $this->attachBodyClassFilter();
         $this->attachTemplateHierarchyFilters();
@@ -83,75 +83,121 @@ class Sage
     /**
      * Attach global `$post` variable to Blade views.
      *
+     * @param  WP_Post $post
+     * @return void
+     */
+    public function filterPost($post)
+    {
+        $this->view->share('post', $post);
+    }
+
+    /**
+     * Attach the post filter.
+     *
      * @param  integer $priority
      * @return void
      */
-    public function attachPostVariable($priority = 10)
+    protected function attachPostFilter($priority = 10)
     {
-        add_action('the_post', function ($post) {
-            $this->view->share('post', $post);
-        }, $priority);
+        add_action('the_post', [$this, 'filterPost'], $priority);
     }
 
     /**
      * Search for a compiled Blade partial when resolving the comments template.
      *
-     * @param  integer $priority
-     * @return void
+     * @param  string $file
+     * @return string
      */
-    public function attachCommentsTemplateResolver($priority = 100)
+    public function filterCommentsTemplate($file)
     {
-        add_filter('comments_template', function ($name) {
-            if (Str::startsWith($file, [STYLESHEETPATH, TEMPLATEPATH])) {
-                $file = ltrim(str_replace([STYLESHEETPATH, TEMPLATEPATH], '', $file), '\\/');
-            }
+        if (Str::startsWith($file, [STYLESHEETPATH, TEMPLATEPATH])) {
+            $file = ltrim(str_replace([STYLESHEETPATH, TEMPLATEPATH], '', $file), '\\/');
+        }
 
-            if ($file == '/comments.php') {
-                $file = '/partials/comments.blade.php';
-            }
+        if ($file == 'comments.php') {
+            $file = 'partials/comments.blade.php';
+        }
 
-            return view(
-                $this->fileFinder->getPossibleViewNameFromPath($file)
-            )->makeLoader();
-        }, $priority);
+        return view(
+            $this->fileFinder->getPossibleViewNameFromPath($file)
+        )->makeLoader();
     }
 
     /**
-     * Use search.blade.php for the search form.
+     * Attach the comments template filter.
      *
      * @param  integer $priority
      * @return void
      */
-    public function attachSearchFormFilter($priority = 100)
+    protected function attachCommentsTemplateFilter($priority = 100)
     {
-        add_filter('get_search_form', function () {
-            return view('forms.search');
-        }, $priority);
+        add_filter('comments_template', [$this, 'filterCommentsTemplate'], $priority);
+    }
+
+    /**
+     * Use `forms/search.blade.php` for the search form.
+     *
+     * @return string
+     */
+    public function filterSearchForm()
+    {
+        return view('forms.search');
+    }
+
+    /**
+     * Attach the search form filter.
+     *
+     * @param  integer $priority
+     * @return void
+     */
+    protected function attachSearchFormFilter($priority = 100)
+    {
+        add_filter('get_search_form', [$this, 'filterSearchForm'], $priority);
     }
 
     /**
      * Clean up the body class and append the page slug.
      *
+     * @param  array $classes
+     * @return array
+     */
+    public function filterBodyClass($classes)
+    {
+        $classes = collect($classes);
+
+        if (is_single() || is_page() && ! is_front_page()) {
+            if (! $classes->contains($class = basename(get_permalink()))) {
+                $classes->push($class);
+            }
+        }
+
+        return $classes->map(function ($class) {
+            return preg_replace(['/-blade(-php)?$/', '/^page-template-views/'], '', $class);
+        })
+        ->filter()
+        ->all();
+    }
+
+    /**
+     * Attach the body class filter.
+     *
      * @param  integer $priority
      * @return void
      */
-    public function attachBodyClassFilter($priority = 10)
+    protected function attachBodyClassFilter($priority = 10)
     {
-        add_filter('body_class', function (array $classes) {
-            /** Add page slug if it doesn't exist */
-            if (is_single() || is_page() && ! is_front_page()) {
-                if (! in_array(basename(get_permalink()), $classes)) {
-                    $classes[] = basename(get_permalink());
-                }
-            }
+        add_filter('body_class', [$this, 'filterBodyClass'], $priority);
+    }
 
-            /** Clean up class names for custom templates */
-            $classes = array_map(function ($class) {
-                return preg_replace(['/-blade(-php)?$/', '/^page-template-views/'], '', $class);
-            }, $classes);
-
-            return array_filter($classes);
-        }, $priority);
+    /**
+     * Use compiled Blade view when returning a template.
+     *
+     * @param  array $files
+     * @return void
+     */
+    public function filterTemplateHierarchy($files)
+    {
+        return $this->sageFinder->locate($files);
     }
 
     /**
@@ -160,7 +206,7 @@ class Sage
      * @param  integer $priority
      * @return void
      */
-    public function attachTemplateHierarchyFilters($priority = 10)
+    protected function attachTemplateHierarchyFilters($priority = 10)
     {
         add_filters([
             'index_template_hierarchy',
@@ -179,33 +225,40 @@ class Sage
             'single_template_hierarchy',
             'singular_template_hierarchy',
             'attachment_template_hierarchy',
-        ], function ($files) {
-            return $this->sageFinder->locate($files);
-        }, $priority);
+        ], [$this, 'filterTemplateHierarchy'], $priority);
     }
 
     /**
      * Include compiled Blade view with data attached.
      *
+     * @param  string
+     * @return string
+     */
+    public function filterTemplateInclude($file)
+    {
+        $view = $this->fileFinder
+            ->getPossibleViewNameFromPath(realpath($file));
+
+        /** Gather data to be passed to view */
+        $data = array_reduce(get_body_class(), function ($data, $class) use ($view, $file) {
+            return apply_filters("sage/template/{$class}/data", $data, $view, $file);
+        }, []);
+
+        $this->app['sage.view'] = $view;
+        $this->app['sage.data'] = $data;
+
+        return get_template_directory() . '/index.php';
+    }
+
+    /**
+     * Attach the template include filter.
+     *
      * @param  integer $priority
      * @return void
      */
-    public function attachTemplateIncludeFilter($priority = 100)
+    protected function attachTemplateIncludeFilter($priority = 100)
     {
-        add_filter('template_include', function ($file) {
-            $view = $this->fileFinder
-                ->getPossibleViewNameFromPath(realpath($file));
-
-            /** Gather data to be passed to view */
-            $data = array_reduce(get_body_class(), function ($data, $class) use ($view, $file) {
-                return apply_filters("sage/template/{$class}/data", $data, $view, $file);
-            }, []);
-
-            $this->app['sage.view'] = $view;
-            $this->app['sage.data'] = $data;
-
-            return get_template_directory() . '/index.php';
-        }, $priority);
+        add_filter('template_include', [$this, 'filterTemplateInclude'], $priority);
     }
 
     /**
@@ -213,49 +266,58 @@ class Sage
      *
      * @return void
      */
-    public function attachThemeTemplatesFilter($priority = 100)
+    public function filterThemeTemplates($_templates, $_theme, $_post, $post_type)
     {
-        add_filter('theme_templates', function ($_templates, $_theme, $_post, $post_type) {
-            $templates = [];
+        $templates = [];
 
-            foreach (array_reverse($this->fileFinder->getPaths()) as $path) {
-                /**
-                 * We use the exact same technique as WordPress core for detecting template files.
-                 *
-                 * Caveat: we go infinite levels deep within the views folder.
-                 *
-                 * @see \WP_Theme::get_post_templates()
-                 * @link https://github.com/WordPress/WordPress/blob/5.2.1/wp-includes/class-wp-theme.php#L1146-L1164
-                 */
-                foreach ($this->files->glob("{$path}/**.php") as $full_path) {
-                    if (! preg_match('|Template Name:(.*)$|mi', file_get_contents($full_path), $header)) {
-                        continue;
+        foreach (array_reverse($this->fileFinder->getPaths()) as $path) {
+            /**
+             * We use the exact same technique as WordPress core for detecting template files.
+             *
+             * Caveat: we go infinite levels deep within the views folder.
+             *
+             * @see \WP_Theme::get_post_templates()
+             * @link https://github.com/WordPress/WordPress/blob/5.2.1/wp-includes/class-wp-theme.php#L1146-L1164
+             */
+            foreach ($this->files->glob("{$path}/**.php") as $full_path) {
+                if (! preg_match('|Template Name:(.*)$|mi', file_get_contents($full_path), $header)) {
+                    continue;
+                }
+
+                $types = ['page'];
+
+                if (preg_match('|Template Post Type:(.*)$|mi', file_get_contents($full_path), $type)) {
+                    $types = explode(',', _cleanup_header_comment($type[1]));
+                }
+
+                $file = $this->files->getRelativePath("{$path}/", $full_path);
+
+                foreach ($types as $type) {
+                    $type = sanitize_key($type);
+
+                    if (! isset($templates[$type])) {
+                        $templates[$type] = [];
                     }
 
-                    $types = ['page'];
-
-                    if (preg_match('|Template Post Type:(.*)$|mi', file_get_contents($full_path), $type)) {
-                        $types = explode(',', _cleanup_header_comment($type[1]));
-                    }
-
-                    $file = $this->files->getRelativePath("{$path}/", $full_path);
-
-                    foreach ($types as $type) {
-                        $type = sanitize_key($type);
-
-                        if (! isset($templates[$type])) {
-                            $templates[$type] = [];
-                        }
-
-                        $templates[$type][$file] = _cleanup_header_comment($header[1]);
-                    }
+                    $templates[$type][$file] = _cleanup_header_comment($header[1]);
                 }
             }
+        }
 
-            return collect($_templates)
-                ->merge($templates[$post_type] ?? [])
-                ->unique()
-                ->toArray();
-        }, $priority, 4);
+        return collect($_templates)
+            ->merge($templates[$post_type] ?? [])
+            ->unique()
+            ->toArray();
+    }
+
+    /**
+     * Attach the theme templates filter.
+     *
+     * @param  integer $priority
+     * @return void
+     */
+    protected function attachThemeTemplatesFilter($priority = 100)
+    {
+        add_filter('theme_templates', [$this, 'filterThemeTemplates'], $priority, 4);
     }
 }
