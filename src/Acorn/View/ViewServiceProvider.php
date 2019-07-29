db4663a730f1666844e7f5c17db82b555ad2c304
Brandon <brandon@tendency.me>
2019-07-29 02:15:58 -0500

    Add View Composer autoloading

--- src/Acorn/View/ViewServiceProvider.php
+++ src/Acorn/View/ViewServiceProvider.php
@@ -2,33 +2,70 @@
 
 namespace Roots\Acorn\View;
 
+use ReflectionClass;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 use Illuminate\View\ViewServiceProvider as ViewServiceProviderBase;
+use Symfony\Component\Finder\Finder;
 use Roots\Acorn\View\Composers\Debugger;
 use Roots\Acorn\View\Directives\InjectionDirective;
 
 class ViewServiceProvider extends ViewServiceProviderBase
 {
+    /**
+     * Register services.
+     *
+     * @return void
+     */
     public function register()
     {
         parent::register();
         $this->registerMacros();
     }
 
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->attachDirectives();
+        $this->attachComponents();
+        $this->attachComposers();
+
+        if ($this->app['config']['view.debug']) {
+            $this->attachDebugger();
+        }
+    }
+
+    /**
+     * Register View Finder
+     *
+     * @return void
+     */
     public function registerViewFinder()
     {
         $this->app->bind('view.finder', function ($app) {
             $finder = new FileViewFinder($app['files'], array_unique($app['config']['view.paths']));
+
             foreach ($app['config']['view.namespaces'] as $namespace => $hints) {
                 $finder->addNamespace($namespace, $hints);
             }
+
             return $finder;
         });
+
         $this->app->alias('view.finder', FileViewFinder::class);
     }
 
+    /**
+     * Register View Macros
+     *
+     * @return void
+     */
     public function registerMacros()
     {
         /**
@@ -58,24 +95,21 @@ class ViewServiceProvider extends ViewServiceProviderBase
             $compiled = $this->getCompiled();
             $id = basename($compiled, '.php');
             $loader = dirname($compiled) . "/{$id}-loader.php";
+
             if (! file_exists($loader)) {
                 file_put_contents($loader, "<?= \\Roots\\view('{$view}', \$data ?? get_defined_vars()); ?>");
             }
+
             return $loader;
         });
     }
 
-    public function boot()
-    {
-        $this->attachDirectives();
-        $this->attachComponents();
-        $this->attachComposers();
-
-        if ($this->app['config']['view.debug']) {
-            $this->attachDebugger();
-        }
-    }
-
+    /**
+     * Preflight
+     *
+     * @param  Filesystem $files
+     * @return void
+     */
     public function preflight(Filesystem $files)
     {
         $compiled_dir = $this->app['config']['view.compiled'];
@@ -85,39 +119,88 @@ class ViewServiceProvider extends ViewServiceProviderBase
         }
     }
 
+    /**
+     * Attach View Directives
+     *
+     * @return void
+     */
     public function attachDirectives()
     {
         $blade = $this->app['view']->getEngineResolver()->resolve('blade')->getCompiler();
         $directives = $this->app['config']['view.directives'];
         $directives += ['inject' => InjectionDirective::class];
+
         foreach ($directives as $name => $handler) {
             if (! is_callable($handler)) {
                 $handler = $this->app->make($handler);
             }
+
             $blade->directive($name, $handler);
         }
     }
 
+    /**
+     * Attach View Components
+     *
+     * @return void
+     */
     public function attachComponents()
     {
         $components = $this->app->config['view.components'];
 
         if (is_array($components) && Arr::isAssoc($components)) {
             $blade = $this->app['view']->getEngineResolver()->resolve('blade')->getCompiler();
+
             foreach ($components as $alias => $view) {
                 $blade->component($view, $alias);
             }
         }
     }
 
+    /**
+     * Attach View Composers
+     *
+     * @return void
+     */
     public function attachComposers()
     {
         $view = $this->app['view'];
+
         foreach ($this->app->config['view.composers'] as $composer) {
             $view->composer($composer::views(), $composer);
         }
+
+        $paths = collect($this->app->path('Composers'))
+            ->unique()
+            ->filter(function ($path) {
+                return is_dir($path);
+            });
+
+        if ($paths->isEmpty()) {
+            return;
+        }
+
+        $namespace = $this->app->getNamespace();
+
+        foreach ((new Finder())->in($paths->all())->files() as $composer) {
+            $composer = $namespace . str_replace(
+                ['/', '.php'],
+                ['\\', ''],
+                Str::after($composer->getPathname(), realpath($this->app->path()) . DIRECTORY_SEPARATOR)
+            );
+
+            if (is_subclass_of($composer, Composer::class) &&
+                ! (new ReflectionClass($composer))->isAbstract()) {
+                $view->composer($composer::views(), $composer);
+            }
+        }
     }
 
+    /**
+     * Attach View Debugger
+     *
+     * @return void
+     */
     public function attachDebugger()
     {
         $this->app['view']->composer('*', Debugger::class);
