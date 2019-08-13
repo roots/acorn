f37750b434816b6201b18853529bea88f1a8d17f
Kelly Mears <developers@tinypixel.dev>
2019-08-13 12:18:30 -0700

    feat(view): Add view composer traits

---
+++ src/Acorn/View/Composers/Concerns/AcfFields.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Roots\Acorn\View\Composers\Concerns;
+
+use Illuminate\Support\Fluent;
+
+trait AcfFields
+{
+    /**
+     * ACF data to be passed to view before rendering
+     *
+     * @return array
+     */
+    protected function fields($post_id = null)
+    {
+        $acf_data = \get_fields($post_id);
+
+        return array_map(function ($value, $key) {
+            $value = is_array($value) ? new Fluent($value) : $value;
+            return method_exists($this, $key) ? $this->{$key}($value) : $value;
+        }, $acf_data, array_keys($acf_data));
+    }
+}
