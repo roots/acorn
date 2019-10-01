d1f8b68151c9931f97fe0e2e1ceef6e5c17be9f4
QWp6t <QWp6t@users.noreply.github.com>
2019-10-01 04:24:39 -0700

    fix(view-composers): ACF should have snake case keys

--- src/Acorn/View/Composers/Concerns/AcfFields.php
+++ src/Acorn/View/Composers/Concerns/AcfFields.php
@@ -18,7 +18,8 @@ trait AcfFields
         return collect(\get_fields($post_id))
             ->mapWithKeys(function ($value, $key) {
                 $value = is_array($value) ? new Fluent($value) : $value;
-                return [$key => method_exists($this, $key = Str::camel($key)) ? $this->{$key}($value) : $value];
+                $method = Str::camel($key);
+                return [$key => method_exists($this, $method) ? $this->{$method}($value) : $value];
             })->all();
     }
 }
