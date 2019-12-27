3b4b1b4889c14357bdcabdb1cb9b43c12baf4fd9
QWp6t <QWp6t@users.noreply.github.com>
2019-12-27 08:33:15 -0800

    fix(view): Call ->render() instead of toString()
    x-ref: https://github.com/roots/sage/pull/2330

--- src/Acorn/View/ViewServiceProvider.php
+++ src/Acorn/View/ViewServiceProvider.php
@@ -111,7 +111,7 @@ class ViewServiceProvider extends ViewServiceProviderBase
             $loader = dirname($compiled) . "/{$id}-loader.php";
 
             if (! file_exists($loader)) {
-                file_put_contents($loader, "<?= \\Roots\\view('{$view}', \$data ?? get_defined_vars()); ?>");
+                file_put_contents($loader, "<?= \\Roots\\view('{$view}', \$data ?? get_defined_vars())->render(); ?>");
             }
 
             return $loader;
