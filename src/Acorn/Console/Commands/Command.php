73f90b4cc36b2016d14d7467fae86c9a92e52f17
Aaron Collegeman <aaron@collegeman.net>
2020-12-21 10:43:29 -0500

    Update Command.php

--- src/Acorn/Console/Commands/Command.php
+++ src/Acorn/Console/Commands/Command.php
@@ -3,6 +3,10 @@
 namespace Roots\Acorn\Console\Commands;
 
 use Illuminate\Console\Command as CommandBase;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Illuminate\Console\Parser;
 
 abstract class Command extends CommandBase
 {
@@ -24,5 +28,51 @@ abstract class Command extends CommandBase
     public function setLaravel($laravel)
     {
         parent::setLaravel($this->app = $laravel);
+    }
+    
+    /**
+     * Configure the console command using a fluent definition.
+     *
+     * @return void
+     */
+    protected function configureUsingFluentDefinition()
+    {
+        [$name, $arguments, $options] = Parser::parse($this->signature);
+        
+        // Add option --wp-blog to all signatures
+        $options[] = new InputOption(
+            'wp-blog',
+            null, // shortcut
+            InputOption::VALUE_OPTIONAL,
+            'For multsite environments, either a Blog ID or Blog Domain to switch to before execution',
+            null // default
+        );
+
+        parent::__construct($this->name = $name);
+
+        // After parsing the signature we will spin through the arguments and options
+        // and set them on this command. These will already be changed into proper
+        // instances of these "InputArgument" and "InputOption" Symfony classes.
+        $this->getDefinition()->addArguments($arguments);
+        $this->getDefinition()->addOptions($options);
+    }
+    
+    /**
+     * Execute the console command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->hasOption('wp-blog')) {
+            $blog = $input->getOption('wp-blog');
+            if (!is_numeric($blog)) {
+                $blog = get_blog_id_from_url($blog);   
+            }
+            switch_to_blog($blog);
+        }
+        return (int) $this->laravel->call([$this, 'handle']);
     }
 }
