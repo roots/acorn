b0f95a33c816480c2ce6547ed1e3d18045688265
Stefan Fisk <contact@stefanfisk.com>
2020-12-23 09:16:13 +0100

    Bind paths in container
    Fixes #82.

--- src/Acorn/Application.php
+++ src/Acorn/Application.php
@@ -67,11 +67,44 @@ class Application extends Container implements ApplicationContract
     public function __construct($basePath = null)
     {
         if ($basePath) {
-            $this->basePath = $basePath;
+            $this->setBasePath($basePath);
         }
 
         $this->registerContainerBindings();
         $this->bootstrapContainer();
+    }
+
+    /**
+     * Set the base path for the application.
+     *
+     * @param  string $basePath
+     * @return $this
+     */
+    public function setBasePath($basePath)
+    {
+        $this->basePath = rtrim($basePath, '\/');
+
+        $this->bindPathsInContainer();
+
+        return $this;
+    }
+
+    /**
+     * Bind all of the application paths in the container.
+     *
+     * @return void
+     */
+    protected function bindPathsInContainer()
+    {
+        $this->instance('path', $this->path());
+        $this->instance('path.base', $this->basePath());
+        $this->instance('path.lang', $this->langPath());
+        $this->instance('path.config', $this->configPath());
+        $this->instance('path.public', $this->publicPath());
+        $this->instance('path.storage', $this->storagePath());
+        $this->instance('path.database', $this->databasePath());
+        $this->instance('path.resources', $this->resourcePath());
+        $this->instance('path.bootstrap', $this->bootstrapPath());
     }
 
     /**
