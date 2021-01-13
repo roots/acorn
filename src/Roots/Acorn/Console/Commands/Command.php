e293e621f4eaf41aadfa543fe971dfd8805909d3
Brandon <brandon@tendency.me>
2021-01-13 16:57:45 -0600

    wip

--- src/Roots/Acorn/Console/Commands/Command.php
+++ src/Roots/Acorn/Console/Commands/Command.php
@@ -3,8 +3,22 @@
 namespace Roots\Acorn\Console\Commands;
 
 use Illuminate\Console\Command as CommandBase;
+use Illuminate\Contracts\Foundation\Application;
 
 abstract class Command extends CommandBase
 {
-    //
+    /**
+     * The application implementation.
+     *
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setLaravel($laravel)
+    {
+        parent::setLaravel($this->app = $laravel);
+    }
 }
