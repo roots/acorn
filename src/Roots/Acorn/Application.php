a831ff6cce95e99f1d5fdf0d5313a46353c49d2b
QWp6t <hi@qwp6t.me>
2021-02-02 22:00:58 -0800

    feat(app): make all application paths configurable

--- src/Roots/Acorn/Application.php
+++ src/Roots/Acorn/Application.php
@@ -69,6 +69,10 @@ class Application extends FoundationApplication
      */
     public function __construct($basePath = null, $paths = null)
     {
+        if ($paths) {
+            $this->usePaths((array)$paths);
+        }
+
         $this->registerLazyLoader();
 
         parent::__construct($basePath);
@@ -82,6 +86,185 @@ class Application extends FoundationApplication
     protected function registerLazyLoader()
     {
         $this->instances['app.lazy'] = new LazyLoader($this);
+    }
+
+    /**
+     * Set paths that are configurable by the developer.
+     *
+     * Supported path types:
+     * - app
+     * - bootstrap
+     * - config
+     * - database
+     * - lang
+     * - public
+     * - resources
+     * - storage
+     *
+     * @param  array  $path
+     * @return $this
+     */
+    public function usePaths(array $paths)
+    {
+        $supported_paths = [
+            'app' => 'appPath',
+            'lang' => 'langPath',
+            'config' => 'configPath',
+            'public' => 'publicPath',
+            'storage' => 'storagePath',
+            'database' => 'databasePath',
+            'resources' => 'resourcesPath',
+            'bootstrap' => 'bootstrapPath',
+        ];
+
+        foreach ($paths as $path_type => $path) {
+            $path = rtrim($path, '\\/');
+
+            if (!isset($supported_paths[$path_type])) {
+                throw new Exception("The {$path_type} path type is not supported.");
+            }
+
+            if (!is_dir($path) || !is_readable($path)) {
+                throw new Exception("The {$path} directory must be present.");
+            }
+
+            $this->{$supported_paths[$path_type]} = $path;
+        }
+
+        $this->bindPathsInContainer();
+
+        return $this;
+    }
+
+    /**
+     * Get the path to the bootstrap directory.
+     *
+     * @param  string  $path Optionally, a path to append to the bootstrap path
+     * @return string
+     */
+    public function bootstrapPath($path = '')
+    {
+        return ($this->bootstrapPath ?: $this->basePath . DIRECTORY_SEPARATOR . 'bootstrap')
+            . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
+
+    /**
+     * Set the bootstrap directory.
+     *
+     * @param  string  $path
+     * @return $this
+     */
+    public function useBootstrapPath($path)
+    {
+        $this->bootstrapPath = $path;
+
+        $this->instance('path.bootstrap', $path);
+
+        return $this;
+    }
+
+    /**
+     * Get the path to the application configuration files.
+     *
+     * @param  string  $path Optionally, a path to append to the config path
+     * @return string
+     */
+    public function configPath($path = '')
+    {
+        return ($this->configPath ?: $this->basePath . DIRECTORY_SEPARATOR . 'config')
+            . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
+
+    /**
+     * Set the config directory.
+     *
+     * @param  string  $path
+     * @return $this
+     */
+    public function useConfigPath($path)
+    {
+        $this->configPath = $path;
+
+        $this->instance('path.config', $path);
+
+        return $this;
+    }
+
+    /**
+     * Get the path to the language files.
+     *
+     * @return string
+     */
+    public function langPath()
+    {
+        return $this->langPath ?: $this->resourcePath('lang');
+    }
+
+    /**
+     * Set the path to the language files.
+     *
+     * @param  string  $path
+     * @return $this
+     */
+    public function useLangPath($path)
+    {
+        $this->langPath = $path;
+
+        $this->instance('path.lang', $path);
+
+        return $this;
+    }
+
+    /**
+     * Get the path to the public / web directory.
+     *
+     * @return string
+     */
+    public function publicPath()
+    {
+        return $this->publicPath ?: $this->basePath . DIRECTORY_SEPARATOR . 'public';
+    }
+
+    /**
+     * Set the public directory.
+     *
+     * @param  string  $path
+     * @return $this
+     */
+    public function usePublicPath($path)
+    {
+        $this->publicPath = $path;
+
+        $this->instance('path.public', $path);
+
+        return $this;
+    }
+
+    /**
+     * Get the path to the resources directory.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    public function resourcePath($path = '')
+    {
+        return ($this->resourcesPath ?: $this->basePath . DIRECTORY_SEPARATOR . 'resources')
+            . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
+
+    /**
+     * Set the resources directory.
+     *
+     * @param  string  $path
+     * @return $this
+     */
+    public function useResourcePath($path)
+    {
+        $this->resourcesPath = $path;
+
+        $this->instance('path.resources', $path);
+
+        return $this;
     }
 
     /**
