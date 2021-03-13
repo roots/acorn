7cd04dcad26b9680d3ac88bf922273df6db6668b
QWp6t <hi@qwp6t.me>
2021-03-12 23:09:06 -0800

    build(test): add snapshots support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,4 +15,15 @@ class TestCase extends MockeryTestCase
 
         return __DIR__ . DIRECTORY_SEPARATOR . $basepath . DIRECTORY_SEPARATOR . '__fixtures__' . DIRECTORY_SEPARATOR . $path;
     }
+
+    /* this does not work but maybe one day it will 🥺 */
+    protected function getSnapshotDirectory(): string
+    {
+        $basepath = trim(preg_replace([
+            '/' . class_basename($this) . '$/',
+            '/^P\\\Tests/',
+        ], '', get_class($this)), '\\/');
+
+        return __DIR__ . DIRECTORY_SEPARATOR . $basepath . DIRECTORY_SEPARATOR . '__snapshots__';
+    }
 }
