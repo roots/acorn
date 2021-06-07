223bd86f8f6a2b0663168e947e1f031fd7a02d32
QWp6t <hi@qwp6t.me>
2021-06-06 19:46:20 -0700

    chore(helpers): deprecate namespaced helpers

--- src/helpers.php
+++ src/helpers.php
@@ -46,3 +46,187 @@ function bootloader($callback = null)
         $bootloader->call($callback);
     }
 }
+
+/**
+ * @deprecated
+ */
+function app(...$args)
+{
+    return \app(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function app_path(...$args)
+{
+    return \app_path(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function base_path(...$args)
+{
+    return \base_path(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function bcrypt(...$args)
+{
+    return \bcrypt(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function broadcast(...$args)
+{
+    return \broadcast(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function cache(...$args)
+{
+    return \cache(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function config(...$args)
+{
+    return \config(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function config_path(...$args)
+{
+    return \config_path(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function database_path(...$args)
+{
+    return \database_path(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function decrypt(...$args)
+{
+    return \decrypt(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function encrypt(...$args)
+{
+    return \encrypt(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function info(...$args)
+{
+    return \info(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function logger(...$args)
+{
+    return \logger(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function logs(...$args)
+{
+    return \logs(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function now(...$args)
+{
+    return \now(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function public_path(...$args)
+{
+    return \public_path(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function report(...$args)
+{
+    return \report(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function rescue(...$args)
+{
+    return \rescue(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function resolve(...$args)
+{
+    return \resolve(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function resource_path(...$args)
+{
+    return \resource_path(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function storage_path(...$args)
+{
+    return \storage_path(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function today(...$args)
+{
+    return \today(...$args);
+}
+
+/**
+ * @deprecated
+ */
+function view(...$args)
+{
+    return \view(...$args);
+}
