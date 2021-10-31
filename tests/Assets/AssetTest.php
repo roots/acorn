a4e1356cf229366aaaad083b895a938484d58901
QWp6t <hi@qwp6t.me>
2021-10-31 00:13:38 -0700

    test: add more tests

--- tests/Assets/AssetTest.php
+++ tests/Assets/AssetTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Roots\Acorn\Assets\Asset\Asset;
 use Roots\Acorn\Assets\Asset\JsonAsset;
 use Roots\Acorn\Assets\Asset\PhpAsset;
@@ -7,6 +8,7 @@ use Roots\Acorn\Assets\Asset\SvgAsset;
 use Roots\Acorn\Assets\Manifest;
 use Roots\Acorn\Tests\Test\TestCase;
 
+use function Roots\Acorn\Tests\temp;
 use function Spatie\Snapshots\assertMatchesSnapshot;
 
 uses(TestCase::class);
@@ -16,42 +18,58 @@ beforeEach(function () {
     $this->assets = new Manifest($this->fixture('asset_types'), 'https://k.jo', $manifest);
 });
 
-it('can create an asset', fn () =>
-    expect($this->assets->asset('apray.ext'))->toBeInstanceOf(Asset::class)
-);
+it('can create an asset', function () {
+    expect($this->assets->asset('apray.ext'))->toBeInstanceOf(Asset::class);
+});
 
-it('can create a json asset', fn () =>
-    expect($this->assets->asset('kjo.json'))->toBeInstanceOf(JsonAsset::class)
-);
+it('can create a json asset', function () {
+    expect($this->assets->asset('kjo.json'))->toBeInstanceOf(JsonAsset::class);
+});
 
-it('can create a php asset', fn () =>
-    expect($this->assets->asset('bnif.php'))->toBeInstanceOf(PhpAsset::class)
-);
+it('can create a php asset', function () {
+    expect($this->assets->asset('bnif.php'))->toBeInstanceOf(PhpAsset::class);
+});
 
-it('can create a svg asset', fn () =>
-    expect($this->assets->asset('bdubs.svg'))->toBeInstanceOf(SvgAsset::class)
-);
+it('can create a svg asset', function () {
+    expect($this->assets->asset('bdubs.svg'))->toBeInstanceOf(SvgAsset::class);
+});
 
-it('can create a data URL', fn () =>
-    assertMatchesSnapshot($this->assets->asset('bdubs.svg')->dataUrl())
-);
+it('can create a data URL', function () {
+    assertMatchesSnapshot($this->assets->asset('bdubs.svg')->dataUrl());
+});
 
-it('can create a base64 data URL', fn () =>
-    assertMatchesSnapshot($this->assets->asset('apray.ext')->dataUrl())
-);
+it('can create a base64 data URL', function () {
+    assertMatchesSnapshot($this->assets->asset('apray.ext')->dataUrl());
+});
 
-it('can decode json', fn () =>
-    assertMatchesSnapshot($this->assets->asset('kjo.json')->decode())
-);
+it('can decode json', function () {
+    assertMatchesSnapshot($this->assets->asset('kjo.json')->decode());
+});
 
-it('can convert json to array', fn () =>
-    assertMatchesSnapshot($this->assets->asset('kjo.json')->toArray())
-);
+it('can convert json to array', function () {
+    assertMatchesSnapshot($this->assets->asset('kjo.json')->toArray());
+});
 
-it('can re-encode json', fn () =>
-    assertMatchesSnapshot($this->assets->asset('kjo.json')->toJson())
-);
+it('can re-encode json', function () {
+    assertMatchesSnapshot($this->assets->asset('kjo.json')->toJson());
+});
 
-it('can load a php asset', fn () =>
-    assertMatchesSnapshot($this->assets->asset('bnif.php')->load())
-);
+it('can include a php asset', function () {
+    assertMatchesSnapshot($this->assets->asset('bnif.php')->load());
+});
+
+it('can include_once a php asset', function () {
+    assertMatchesSnapshot($this->assets->asset('bnif.php')->load(false, true));
+});
+
+it('can require a php asset', function () {
+    assertMatchesSnapshot($this->assets->asset('bnif.php')->load(true));
+});
+
+it('can require_once a php asset', function () {
+    assertMatchesSnapshot($this->assets->asset('bnif.php')->load(true, true));
+});
+
+it('can fail to load a php asset', function () {
+    (new PhpAsset(temp('does/not/exist.php'), 'https://kjo.kjo/'))->load();
+})->throws(FileNotFoundException::class);
---
+++ tests/Assets/BladeDirectiveTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Roots\Acorn\Assets\View\BladeDirective;
+use Roots\Acorn\Tests\Test\TestCase;
+
+use function Spatie\Snapshots\assertMatchesSnapshot;
+
+uses(TestCase::class);
+
+it('loads an asset', function () {
+    $directive = new BladeDirective();
+    assertMatchesSnapshot($directive('kjo'));
+});
