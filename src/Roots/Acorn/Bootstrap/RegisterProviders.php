c1219d187c52af4e8d8905956b2c7c68a0164a92
QWp6t <QWp6t@users.noreply.github.com>
2021-10-31 15:08:29 -0700

    chore: remove unnecessary substr from `$composer_paths`

--- src/Roots/Acorn/Bootstrap/RegisterProviders.php
+++ src/Roots/Acorn/Bootstrap/RegisterProviders.php
@@ -45,15 +45,10 @@ class RegisterProviders extends FoundationRegisterProviders
                     get_template_directory(),
                     get_stylesheet_directory(),
                 ])
-                ->map(function ($path) {
-                    return "{$path}/vendor/composer/installed.json";
-                })
                 ->unique()
                 ->filter(function ($path) use ($files) {
-                    return $files->exists($path);
-                })
-                ->map(function ($path) {
-                    return substr($path, 0, -strlen('/vendor/composer/installed.json'));
+                    return $files->isFile("{$path}/vendor/composer/installed.json")
+                        && $files->isFile("{$path}/composer.json");
                 })
                 ->all();
 
