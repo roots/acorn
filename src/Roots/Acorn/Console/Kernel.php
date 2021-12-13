d70a4226e5bae56d4753fce5f748feeab5cbf6a8
Stefan Fisk <contact@stefanfisk.com>
2021-12-13 01:08:55 +0100

    fix(console): use upstream `Kernel::load()` method
    Co-authored-by: QWp6t <hi@qwp6t.me>

--- src/Roots/Acorn/Console/Kernel.php
+++ src/Roots/Acorn/Console/Kernel.php
@@ -2,9 +2,6 @@
 
 namespace Roots\Acorn\Console;
 
-use ReflectionClass;
-use Illuminate\Console\Application as Console;
-use Illuminate\Console\Commands\Command;
 use Illuminate\Foundation\Console\ClearCompiledCommand;
 use Illuminate\Foundation\Console\ComponentMakeCommand;
 use Illuminate\Foundation\Console\ConfigClearCommand;
@@ -16,15 +13,12 @@ use Illuminate\Foundation\Console\ProviderMakeCommand;
 use Illuminate\Foundation\Console\VendorPublishCommand;
 use Illuminate\Foundation\Console\ViewCacheCommand;
 use Illuminate\Foundation\Console\ViewClearCommand;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Roots\Acorn\Application;
 use Roots\Acorn\Console\Commands\ComposerMakeCommand;
 use Roots\Acorn\Console\Commands\ConfigCacheCommand;
 use Roots\Acorn\Console\Commands\OptimizeClearCommand;
 use Roots\Acorn\Console\Commands\OptimizeCommand;
 use Roots\Acorn\Console\Commands\SummaryCommand;
-use Symfony\Component\Finder\Finder;
 
 class Kernel extends FoundationConsoleKernel
 {
@@ -73,43 +67,5 @@ class Kernel extends FoundationConsoleKernel
     public function commands()
     {
         $this->load($this->app->path('Console/Commands'));
-    }
-
-    /**
-     * Register all of the commands in the given directory.
-     *
-     * @param  array|string  $paths
-     * @return void
-     */
-    protected function load($paths)
-    {
-        $paths = array_unique(Arr::wrap($paths));
-
-        $paths = array_filter($paths, function ($path) {
-            return is_dir($path);
-        });
-
-        if (empty($paths)) {
-            return;
-        }
-
-        $namespace = $this->app->getNamespace();
-
-        foreach ((new Finder())->in($paths)->files() as $command) {
-            $command = $namespace . str_replace(
-                ['/', '.php'],
-                ['\\', ''],
-                Str::after($command->getPathname(), realpath($this->app->path()) . DIRECTORY_SEPARATOR)
-            );
-
-            if (
-                is_subclass_of($command, Command::class) &&
-                ! (new ReflectionClass($command))->isAbstract()
-            ) {
-                Console::starting(function ($artisan) use ($command) {
-                    $artisan->resolve($command);
-                });
-            }
-        }
     }
 }
