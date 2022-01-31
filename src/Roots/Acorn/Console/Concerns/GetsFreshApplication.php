07a5953ca68f83692e83f50f59a1f3d79ffd0e7a
QWp6t <hi@qwp6t.me>
2022-01-30 21:37:42 -0800

    🐛 use bootloader to get fresh config in config:cache

---
+++ src/Roots/Acorn/Console/Concerns/GetsFreshApplication.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Roots\Acorn\Console\Concerns;
+
+use Roots\Acorn\Bootloader;
+
+trait GetsFreshApplication
+{
+    /**
+     * Get a fresh application instance.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application
+     */
+    protected function getFreshApplication()
+    {
+        $bootloaderClass = get_class(Bootloader::getInstance());
+
+        return (new $bootloaderClass)->getApplication();
+    }
+
+    /**
+     * Boot a fresh copy of the application configuration.
+     *
+     * @return array
+     */
+    protected function getFreshConfiguration()
+    {
+        return $this->getFreshApplication()->make('config')->all();
+    }
+}
