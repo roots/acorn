0c691e327875e6d982132f17bbd53972f12efc5a
QWp6t <hi@qwp6t.me>
2022-01-30 20:44:27 -0800

    🔧 use `get_theme_file_path` for default manifest path

--- config/assets.php
+++ config/assets.php
@@ -34,8 +34,8 @@ return [
         'theme' => [
             'path' => get_theme_file_path('public'),
             'url' => get_theme_file_uri('public'),
-            'assets' => public_path('manifest.json'),
-            'bundles' => public_path('entrypoints.json'),
+            'assets' => get_theme_file_path('public/manifest.json'),
+            'bundles' => get_theme_file_path('public/entrypoints.json'),
         ]
     ]
 ];
