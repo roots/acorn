4d9ccaf21535db7d07a0fd0cd029131d9af28f5f
QWp6t <hi@qwp6t.me>
2022-02-13 12:32:11 -0800

    🩹 prevent looping acorn:init command

--- src/Roots/Acorn/Console/Commands/VendorPublishCommand.php
+++ src/Roots/Acorn/Console/Commands/VendorPublishCommand.php
@@ -17,16 +17,32 @@ class VendorPublishCommand extends FoundationVendorPublishCommand
     protected function publishItem($from, $to)
     {
         if (Str::startsWith($to, $vendor_path = dirname(__DIR__, 5))) {
-            $this->info("Cannot publish [{$from}] until Acorn is initialized.");
-
-            if (! $this->confirm("Would you like to initialize Acorn right now?", true)) {
-                throw new \Exception("Please run wp acorn acorn:init");
-            }
-
-            $this->call('acorn:init', ['--base' => $this->getLaravel()->basePath()]);
             $to = str_replace($vendor_path, $this->getLaravel()->basePath(), $to);
+
+            $this->callAcornInit($from, $to);
         }
 
         parent::publishItem($from, $to);
+    }
+
+    /**
+     * Call acorn:init if item cannot be published.
+     *
+     * @param string $from
+     * @param string $to
+     */
+    protected function callAcornInit($from, $to)
+    {
+        if (is_dir(dirname($to))) {
+            return;
+        }
+
+        $this->info("Cannot publish [{$from}] until Acorn is initialized.");
+
+        if (! $this->confirm("Would you like to initialize Acorn right now?", true)) {
+            throw new \Exception("Please run wp acorn acorn:init");
+        }
+
+        $this->call('acorn:init', ['--base' => $this->getLaravel()->basePath()]);
     }
 }
