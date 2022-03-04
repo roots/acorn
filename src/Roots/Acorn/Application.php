c8514709db1232eced199155a96a504e016b4fbe
QWp6t <hi@qwp6t.me>
2022-03-04 07:31:15 -0800

    🔖 v2.0.3

--- src/Roots/Acorn/Application.php
+++ src/Roots/Acorn/Application.php
@@ -28,7 +28,7 @@ class Application extends FoundationApplication
      *
      * @var string
      */
-    public const VERSION = '2.0.2';
+    public const VERSION = '2.0.3';
 
     /**
      * The custom bootstrap path defined by the developer.
