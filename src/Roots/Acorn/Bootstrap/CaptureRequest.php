f294ca3570f3fb5f9d3da32b4387c731cf2840f2
QWp6t <QWp6t@users.noreply.github.com>
2022-03-27 10:19:05 -0700

    👽 Enable `$_SERVER['HTTPS']` in console (#215)

--- src/Roots/Acorn/Bootstrap/CaptureRequest.php
+++ src/Roots/Acorn/Bootstrap/CaptureRequest.php
@@ -15,7 +15,25 @@ class CaptureRequest
      */
     public function bootstrap(Application $app)
     {
+        if ($app->runningInConsole()) {
+            $this->enableHttpsInConsole();
+        }
+
         $app->instance('request', \Illuminate\Http\Request::capture());
         Facade::clearResolvedInstance('request');
+    }
+
+    /**
+     * Enable $_SERVER[HTTPS] in a console environment.
+     *
+     * @return void
+     */
+    protected function enableHttpsInConsole()
+    {
+        $enable = apply_filters('acorn/enable_https_in_console', parse_url(get_option('home'), PHP_URL_SCHEME) === 'https');
+
+        if ($enable) {
+            $_SERVER['HTTPS'] = 'on';
+        }
     }
 }
