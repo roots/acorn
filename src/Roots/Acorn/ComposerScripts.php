8e7719447b1ed4c89826bfdabf7a8cf86a3f4788
QWp6t <hi@qwp6t.me>
2022-06-26 05:40:25 -0700

    🚸 display warning when PHP >= 8.1.0 < 8.1.6

--- src/Roots/Acorn/ComposerScripts.php
+++ src/Roots/Acorn/ComposerScripts.php
@@ -17,6 +17,15 @@ class ComposerScripts extends FoundationComposerScripts
     {
         $console = new Console(new Filesystem(), getcwd());
 
+        if (version_compare(PHP_VERSION, '8.1.0', '>=') && version_compare(PHP_VERSION, '8.1.6', '<')) {
+            printf(
+                "\033[93m⚠ PHP %s has a known bug that can impact certain environments. You should consider updating to a more recent version\n%s\n%s.\033[0m\n",
+                PHP_VERSION,
+                '🔗 https://github.com/roots/acorn/issues/217',
+                '🔗 https://github.com/php/php-src/pull/8297'
+            );
+        }
+
         $console->configClear();
         $console->clearCompiled();
     }
