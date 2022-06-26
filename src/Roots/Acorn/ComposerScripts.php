b5c555e66b1f117f2fe47a58e0f6a7ac8865d1e8
QWp6t <hi@qwp6t.me>
2022-06-26 05:50:15 -0700

    💬 fix punctuation

--- src/Roots/Acorn/ComposerScripts.php
+++ src/Roots/Acorn/ComposerScripts.php
@@ -19,7 +19,7 @@ class ComposerScripts extends FoundationComposerScripts
 
         if (version_compare(PHP_VERSION, '8.1.0', '>=') && version_compare(PHP_VERSION, '8.1.6', '<')) {
             printf(
-                "\033[93m⚠ PHP %s has a known bug that can impact certain environments. You should consider updating to a more recent version\n%s\n%s.\033[0m\n",
+                "\033[93m⚠ PHP %s has a known bug that can impact certain environments. You should consider updating to a more recent version.\n%s\n%s\033[0m\n",
                 PHP_VERSION,
                 '🔗 https://github.com/roots/acorn/issues/217',
                 '🔗 https://github.com/php/php-src/pull/8297'
