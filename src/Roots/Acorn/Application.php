3d1c31aaf6284e92344c61936c0419cce03c69e6
Brandon <brandon@tendency.me>
2023-11-16 13:22:41 -0600

    🩹 Fix bootstrap path

--- src/Roots/Acorn/Application.php
+++ src/Roots/Acorn/Application.php
@@ -119,9 +119,20 @@ class Application extends FoundationApplication
      */
     protected function bindPathsInContainer()
     {
-        $bootstrapPath = $this->bootstrapPath();
-        parent::bindPathsInContainer();
-        $this->instance('path.bootstrap', $bootstrapPath);
+        $this->instance('path', $this->path());
+        $this->instance('path.base', $this->basePath());
+        $this->instance('path.config', $this->configPath());
+        $this->instance('path.database', $this->databasePath());
+        $this->instance('path.public', $this->publicPath());
+        $this->instance('path.resources', $this->resourcePath());
+        $this->instance('path.storage', $this->storagePath());
+        $this->instance('path.bootstrap', $this->bootstrapPath());
+
+        $this->useLangPath(value(function () {
+            return is_dir($directory = $this->resourcePath('lang'))
+                ? $directory
+                : $this->basePath('lang');
+        }));
     }
 
     /**
