b7d043f06182f941d9b380ddfdb696cb85cf243a
Brandon <brandon@tendency.me>
2024-01-05 11:20:27 -0600

    ⏪ Revert changes to the `assets` config

--- config/assets.php
+++ config/assets.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => 'bud',
+    'default' => 'theme',
 
     /*
     |--------------------------------------------------------------------------
@@ -31,16 +31,11 @@ return [
     */
 
     'manifests' => [
-        'bud' => [
+        'theme' => [
             'path' => get_theme_file_path('public'),
             'url' => get_theme_file_uri('public'),
             'assets' => get_theme_file_path('public/manifest.json'),
             'bundles' => get_theme_file_path('public/entrypoints.json'),
         ],
-        'vite' => [
-            'path' => get_theme_file_path('public'),
-            'url' => get_theme_file_uri('public'),
-            'bundles' => get_theme_file_path('public/build/manifest.json'),
-        ],
-    ]
+    ],
 ];
