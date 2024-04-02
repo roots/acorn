60f2b894fd7abf266d665b47da49bfec739fd24e
Brandon <brandon@tendency.me>
2024-04-01 20:24:27 -0500

    ✨ Implement the ApplicationBuilder

---
+++ src/Roots/Acorn/Configuration/Middleware.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Roots\Acorn\Configuration;
+
+use Illuminate\Foundation\Configuration\Middleware as FoundationMiddleware;
+
+class Middleware extends FoundationMiddleware
+{
+    /**
+     * Get the middleware aliases.
+     *
+     * @return array
+     */
+    public function getMiddlewareAliases()
+    {
+        return array_merge($this->defaultAliases(), $this->customAliases);
+    }
+
+    /**
+     * Get the global middleware.
+     *
+     * @return array
+     */
+    public function getGlobalMiddleware()
+    {
+        $middleware = $this->global ?: array_values(array_filter([
+            $this->trustHosts ? \Illuminate\Http\Middleware\TrustHosts::class : null,
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Http\Middleware\ValidatePostSize::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        ]));
+
+        $middleware = array_map(function ($middleware) {
+            return isset($this->replacements[$middleware])
+                ? $this->replacements[$middleware]
+                : $middleware;
+        }, $middleware);
+
+        return array_values(array_filter(
+            array_diff(
+                array_unique(array_merge($this->prepends, $middleware, $this->appends)),
+                $this->removals
+            )
+        ));
+    }
+
+    /**
+     * Get the middleware groups.
+     *
+     * @return array
+     */
+    public function getMiddlewareGroups()
+    {
+        $middleware = [
+            'web' => array_values(array_filter([
+                // \Illuminate\Cookie\Middleware\EncryptCookies::class,
+                // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                \Illuminate\Session\Middleware\StartSession::class,
+                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                // \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                $this->authenticatedSessions ? 'auth.session' : null,
+            ])),
+
+            'api' => array_values(array_filter([
+                $this->statefulApi ? \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class : null,
+                $this->apiLimiter ? 'throttle:'.$this->apiLimiter : null,
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            ])),
+        ];
+
+        $middleware = array_merge($middleware, $this->groups);
+
+        foreach ($middleware as $group => $groupedMiddleware) {
+            foreach ($groupedMiddleware as $index => $groupMiddleware) {
+                if (isset($this->groupReplacements[$group][$groupMiddleware])) {
+                    $middleware[$group][$index] = $this->groupReplacements[$group][$groupMiddleware];
+                }
+            }
+        }
+
+        foreach ($this->groupRemovals as $group => $removals) {
+            $middleware[$group] = array_values(array_filter(
+                array_diff($middleware[$group] ?? [], $removals)
+            ));
+        }
+
+        foreach ($this->groupPrepends as $group => $prepends) {
+            $middleware[$group] = array_values(array_filter(
+                array_unique(array_merge($prepends, $middleware[$group] ?? []))
+            ));
+        }
+
+        foreach ($this->groupAppends as $group => $appends) {
+            $middleware[$group] = array_values(array_filter(
+                array_unique(array_merge($middleware[$group] ?? [], $appends))
+            ));
+        }
+
+        return $middleware;
+    }
+
+    /**
+     * Get the default middleware aliases.
+     *
+     * @return array
+     */
+    protected function defaultAliases()
+    {
+        $aliases = [
+            // 'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+            // 'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+            'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+            'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+            // 'can' => \Illuminate\Auth\Middleware\Authorize::class,
+            // 'guest' => \Illuminate\Auth\Middleware\RedirectIfAuthenticated::class,
+            // 'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+            'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+            'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+            'throttle' => $this->throttleWithRedis
+                ? \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class
+                : \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            // 'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        ];
+
+        if (class_exists(\Spark\Http\Middleware\VerifyBillableIsSubscribed::class)) {
+            $aliases['subscribed'] = \Spark\Http\Middleware\VerifyBillableIsSubscribed::class;
+        }
+
+        return $aliases;
+    }
+}
