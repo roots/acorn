dd04baba7ca8e0792319a57b4fe7aaa8bfa11927
Brandon <brandon@tendency.me>
2024-12-05 11:41:35 -0600

    🧑‍💻 Remove the `render` override in the exception handler (Fixes #407) (#418)

--- src/Roots/Acorn/Exceptions/Handler.php
+++ src/Roots/Acorn/Exceptions/Handler.php
@@ -2,31 +2,11 @@
 
 namespace Roots\Acorn\Exceptions;
 
-use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Foundation\Exceptions\Handler as FoundationHandler;
 use Throwable;
 
 class Handler extends FoundationHandler
 {
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
-     */
-    public function render($request, Throwable $e)
-    {
-        if ($e instanceof Responsable) {
-            return $e->toResponse($request);
-        }
-
-        $e = $this->prepareException($this->mapException($e));
-
-        return $this->prepareResponse($request, $e);
-    }
-
     /**
      * Get the default context variables for logging.
      *
