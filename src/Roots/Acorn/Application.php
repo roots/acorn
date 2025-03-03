7f0b21d8da08b1d7142d7e185e758a397654b6e6
Ben Word <ben@benword.com>
2025-03-03 08:45:13 -0500

    🔧 v5.x-dev

--- src/Roots/Acorn/Application.php
+++ src/Roots/Acorn/Application.php
@@ -30,7 +30,7 @@ class Application extends FoundationApplication
      *
      * @var string
      */
-    public const VERSION = '5.0.0';
+    public const VERSION = '5.x-dev';
 
     /**
      * The custom resource path defined by the developer.
